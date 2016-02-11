88940fb9221c6287b07b1a0ad26ab39bad6d5bfc
ssivaraj <ssivaraj@squaretrade.com>
2016-02-11 12:27:43 -0700

    first spring boot build

---
+++ config-server/src/main/java/com/squaretrade/config/server/config/ConfigServerMain.java
@@ -0,0 +1,17 @@
+package com.squaretrade.config.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.squaretrade.config.server")
+@EnableJpaRepositories("com.squaretrade.config.server.dao")
+@EntityScan("com.squaretrade.config.server.bean")
+public class ConfigServerMain {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConfigServerMain.class, args);
+    }
+}
