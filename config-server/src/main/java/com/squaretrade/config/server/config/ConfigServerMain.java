4506b727751a3ec3a072ea2f381820b4e523ea78
ssivaraj <ssivaraj@squaretrade.com>
2016-02-17 09:38:30 -0700

    stateless session authentication

--- config-server/src/main/java/com/squaretrade/config/server/config/ConfigServerMain.java
+++ config-server/src/main/java/com/squaretrade/config/server/config/ConfigServerMain.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -69,6 +70,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().anyRequest().fullyAuthenticated().and().
                 httpBasic().and().
                 csrf().disable();
